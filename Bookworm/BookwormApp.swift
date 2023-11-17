582e5c43478ab6cca43daafb9b4afbaf95dca1bb
Scott Crowder <scrowder@gmail.com>
2023-11-17 14:57:09 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by SCOTT CROWDER on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

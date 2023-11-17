f25e4eccfcd1836e9afaca325e9f00a187d15c4e
Scott Crowder <scrowder@gmail.com>
2023-11-17 17:39:47 -0600

    add file

---
+++ Bookworm/TutorialFiles/SwiftDataExample.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftDataExample.swift
+//  Bookworm
+//
+//  Created by SCOTT CROWDER on 11/17/23.
+//
+
+import SwiftUI
+
+struct SwiftDataExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftDataExample()
+}

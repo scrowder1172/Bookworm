ebe2c7e5fa022eac8b4f70cc1afe3eb0294eb1fc
Scott Crowder <scrowder@gmail.com>
2023-11-17 18:14:44 -0600

    Practice using SwiftData

--- Bookworm/TutorialFiles/SwiftDataExample.swift
+++ Bookworm/TutorialFiles/SwiftDataExample.swift
@@ -6,10 +6,47 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct SwiftDataExample: View {
+    
+    @Query var students: [Student]
+    
+    @Environment(\.modelContext) var modelContext
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Text("Students")
+            List(students) { student in
+                Text(student.name)
+            }
+            .navigationTitle("Classroom")
+            .toolbar {
+                Button("+Students") {
+                    let firstNames: [String] = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                    let lastNames: [String] = ["Granger", "Lovegood", "Potter", "Weasley"]
+                    
+                    let chosenFirstName: String = firstNames.randomElement()!
+                    let chosenLastName: String = lastNames.randomElement()!
+                    
+                    let student = Student(name: "\(chosenFirstName) \(chosenLastName)")
+                    modelContext.insert(student)
+                }
+            }
+            .frame(height: 300)
+            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+            .padding(.horizontal)
+        }
+    }
+}
+
+@Model
+class Student {
+    var id: UUID = UUID()
+    var name: String
+    
+    init(name: String) {
+        self.name = name
     }
 }
 

385ab2dcd46ae98e44e6caeff0a2e323b5763316
Scott Crowder <scrowder@gmail.com>
2023-11-20 16:16:07 -0600

    add emoji rating system and show books on main view

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -17,18 +17,33 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            Text("Book Count: \(books.count)")
-                .navigationTitle("Bookworm")
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button("Add Book", systemImage: "plus") {
-                            showingAddScreen.toggle()
+            List {
+                ForEach(books) {book in
+                    NavigationLink(value: book) {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                            
+                            VStack(alignment: .leading) {
+                                Text(book.title)
+                                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                                Text(book.author)
+                                    .foregroundStyle(.secondary)
+                            }
                         }
                     }
                 }
-                .sheet(isPresented: $showingAddScreen) {
-                    AddBookView()
+            }
+            .navigationTitle("Bookworm")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add Book", systemImage: "plus") {
+                        showingAddScreen.toggle()
+                    }
                 }
+            }
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView()
+            }
         }
     }
 }

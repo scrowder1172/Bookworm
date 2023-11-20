fb6a2384232eee65b1275bdc994f8c6bccd17927
Scott Crowder <scrowder@gmail.com>
2023-11-20 15:00:22 -0600

    add file

---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by SCOTT CROWDER on 11/20/23.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}

859619cae59fc71b44b759d01ec97ba651e9a76a
Scott Crowder <scrowder@gmail.com>
2023-11-20 16:50:41 -0600

    Create detail view to show book review

--- Bookworm/BookDetailView.swift
+++ Bookworm/BookDetailView.swift
@@ -6,13 +6,54 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct BookDetailview: View {
+    
+    let book: Book
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre.uppercased())
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+            
+            Text(book.review)
+                .padding()
+            
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
     }
 }
 
 #Preview {
-    BookDetailview()
+    do {
+        let config: ModelConfiguration = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container: ModelContainer = try ModelContainer(for: Book.self, configurations: config)
+        let example: Book = Book(title: "Test Book", author: "Test Author", genre: "Fantasy", review: "This is a sample review.", rating: 5)
+        
+        return BookDetailview(book: example)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create preview: \(error.localizedDescription)")
+    }
 }
--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -34,6 +34,9 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Bookworm")
+            .navigationDestination(for: Book.self) { book in
+                BookDetailview(book: book)
+            }
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add Book", systemImage: "plus") {

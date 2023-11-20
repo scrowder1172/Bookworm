812bd7d0b93ab9d50640eb65858e18e4f0f1c1c5
Scott Crowder <scrowder@gmail.com>
2023-11-20 16:30:09 -0600

    add file

---
+++ Bookworm/BookDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  BookDetailview.swift
+//  Bookworm
+//
+//  Created by SCOTT CROWDER on 11/20/23.
+//
+
+import SwiftUI
+
+struct BookDetailview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookDetailview()
+}
